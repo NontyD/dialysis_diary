be8fe360b9caa9cf669cc1681c80629b6b77d746
NontyD <nonty@gmail.com>
2025-02-14 03:07:05 +0000

    define the login URL

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import register_user, signup_page
+from .views import login_page
 
 urlpatterns = [
     path('register/', register_user, name='register_user'),
     path('signup/', signup_page, name='signup_page'),
+    path("login/", login_page, name="login_page"),
 ]
