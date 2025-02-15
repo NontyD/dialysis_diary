81a39308f2f04311a1611a92116d2d5721d52996
NontyD <nonty@gmail.com>
2025-02-15 16:48:55 +0000

    add Bootstrap to base.html

--- dialysis_connect/settings.py
+++ dialysis_connect/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-co=%pxti!caco9fy!@j-2away)*&gadx17rh)iu(jaangcd5xh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
--- dialysis_connect/urls.py
+++ dialysis_connect/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from pages import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')), 
 ]
