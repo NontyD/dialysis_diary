0a9bb225d4582b2de5ead891aab79f0b82d7e33e
NontyD <nonty@gmail.com>
2025-02-16 09:50:15 +0000

    add URL for deleting post

--- community/urls.py
+++ community/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import community_page, edit_post
+from .views import community_page, edit_post, delete_post
 
 urlpatterns = [
     path("", community_page, name="community"),
     path("edit/<int:post_id>/", edit_post, name="edit_post"),
+    path("delete/<int:post_id>/", delete_post, name="delete_post"),
 ]
