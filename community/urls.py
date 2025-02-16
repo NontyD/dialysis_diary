088deac6fda6ad0ad3e9972bda0760f591834ae3
NontyD <nonty@gmail.com>
2025-02-16 09:34:52 +0000

    add URL for editing post

--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import community_page
+from .views import community_page, edit_post
 
 urlpatterns = [
     path("", community_page, name="community"),
+    path("edit/<int:post_id>/", edit_post, name="edit_post"),
 ]
