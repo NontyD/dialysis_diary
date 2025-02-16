84e4897fca531da41abaf5c2026a053bd653ebc4
NontyD <nonty@gmail.com>
2025-02-16 00:08:46 +0000

    create view for community

--- community/views.py
+++ community/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Post
 
-# Create your views here.
+@login_required
+def community_page(request):
+    posts = Post.objects.select_related("author").all()  # Fetch posts efficiently
+    return render(request, "community/community.html", {"posts": posts})
