3d853da81f04b73e7213625d1d7981710bd9fd3d
NontyD <nonty@gmail.com>
2025-02-16 20:38:25 +0000

    create views for calendar_app

--- calendar_app/views.py
+++ calendar_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Event
+from .forms import EventForm
+from datetime import datetime
 
-# Create your views here.
+@login_required
+def add_event(request):
+    if request.method == "POST":
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.user = request.user
+            event.save()
+            messages.success(request, "Event added successfully!")
+            return redirect("calendar_view")
+    else:
+        form = EventForm()
+    return render(request, "calendar_app/add_event.html", {"form": form})
+
+@login_required
+def calendar_view(request):
+    events = Event.objects.filter(user=request.user).order_by("date")
+    return render(request, "calendar_app/calendar.html", {"events": events})
+
