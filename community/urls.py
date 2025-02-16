7abd2e3c8b41d4437b3fa19b00ad70a19138a032
NontyD <nonty@gmail.com>
2025-02-16 10:48:24 +0000

    create records app

--- community/urls.py
+++ community/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import community_page, edit_post, delete_post
+from .views import community_page, edit_post, delete_post, add_comment
 
 urlpatterns = [
     path("", community_page, name="community"),
     path("edit/<int:post_id>/", edit_post, name="edit_post"),
     path("delete/<int:post_id>/", delete_post, name="delete_post"),
+    path("comment/<int:post_id>/", add_comment, name="add_comment"),
 ]
