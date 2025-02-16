a3b5f25d8c219c8422f7943e2bd58ae89e66a5cc
NontyD <nonty@gmail.com>
2025-02-16 11:07:00 +0000

    create URL for the records app

---
+++ records/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import add_record, records_list
+
+urlpatterns = [
+    path("add/", add_record, name="add_record"),
+    path("history/", records_list, name="records_list"),
+]
