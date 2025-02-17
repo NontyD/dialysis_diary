eabf7c71b19925c6ee83eb61cc92f781249f7f7e
NontyD <nonty@gmail.com>
2025-02-17 17:11:00 +0000

    define uploads model

--- uploads/models.py
+++ uploads/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UploadedFile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # File belongs to a user
+    file = models.FileField(upload_to="uploads/")  # Store in 'uploads/' folder
+    uploaded_at = models.DateTimeField(auto_now_add=True)  # Timestamp
